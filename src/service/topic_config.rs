7a2b836e1d0ae708f81a3c02ac7f059890946e05
qipingluo <qipingluo@tencent.com>
2024-08-06 11:25:13 +0800

    delete topic

--- src/service/topic_config.rs
+++ src/service/topic_config.rs
@@ -74,6 +74,14 @@ impl TopicConfigManager {
         self.persist()
     }
 
+    pub fn delete_topic(&mut self, topic_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+        let result = self.topic_config_table.remove(topic_name);
+        if result.is_some() {
+            return self.persist();
+        }
+        Ok(())
+    }
+
     fn persist(&self) -> Result<(), Box<dyn std::error::Error>> {
         fs::copy(self.path.as_str(), self.backup_path.as_str())?;
         let data = serde_json::to_string(&self.topic_config_table)?;
@@ -87,8 +95,8 @@ mod test {
     use serde_json::json;
 
     use super::*;
-    use std::sync::Mutex;
     use once_cell::sync::Lazy;
+    use std::sync::Mutex;
 
     static MTX: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
     #[test]
@@ -125,6 +133,24 @@ mod test {
         topic_config_manager
             .add_or_update_topic_config("test1", topic_config)
             .unwrap();
+        fs::remove_file("./topic_config.json").unwrap();
+        fs::remove_file("./topic_config.json.bak").unwrap();
+    }
+
+    #[test]
+    fn test_delete_topic() {
+        let _m = MTX.lock();
+        let mut topic_config_manager = TopicConfigManager::new("./");
+        topic_config_manager.load().unwrap();
+        let topic = TopicConfig {
+            name: "test1".to_string(),
+            topic_type: TopicType::NORMAL,
+        };
+        topic_config_manager
+            .add_or_update_topic_config("test1", topic)
+            .unwrap();
+        topic_config_manager.delete_topic("test1").unwrap();
+        assert!(topic_config_manager.get_topic_config("test1").is_none());
         fs::remove_file("./topic_config.json").unwrap();
         fs::remove_file("./topic_config.json.bak").unwrap();
     }
