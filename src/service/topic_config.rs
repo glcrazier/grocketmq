3bbf9b1f35a60dc0cbe4f11051aba643bf54a79e
qipingluo <qipingluo@tencent.com>
2024-08-06 10:58:54 +0800

    add topic_config_manager

---
+++ src/service/topic_config.rs
@@ -0,0 +1,131 @@
+use std::{collections::HashMap, fs, path::Path};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[repr(u8)]
+pub enum TopicType {
+    NORMAL,
+    DELAY,
+    FIFO,
+    TRANSACTION,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct TopicConfig {
+    name: String,
+    topic_type: TopicType,
+}
+
+impl TopicConfig {
+    pub fn new(name: String, topic_type: TopicType) -> Self {
+        Self { name, topic_type }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn topic_type(&self) -> &TopicType {
+        &self.topic_type
+    }
+}
+
+#[derive(Debug)]
+pub struct TopicConfigManager {
+    path: String,
+    topic_config_table: HashMap<String, TopicConfig>,
+    backup_path: String,
+}
+
+impl TopicConfigManager {
+    pub fn new(path: &str) -> Self {
+        let topic_config_path = path.to_string() + "/topic_config.json";
+        let backup_path = topic_config_path.clone() + ".bak";
+        Self {
+            topic_config_table: HashMap::new(),
+            path: topic_config_path,
+            backup_path,
+        }
+    }
+
+    pub fn load(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let path = Path::new(self.path.as_str());
+        let result = fs::read_to_string(path);
+        if let Ok(data) = result {
+            let topic_config_table: HashMap<String, TopicConfig> = serde_json::from_str(&data)?;
+            self.topic_config_table = topic_config_table;
+        } else {
+            fs::write(path, "{}")?;
+        }
+        Ok(())
+    }
+
+    pub fn get_topic_config(&self, topic_name: &str) -> Option<&TopicConfig> {
+        self.topic_config_table.get(topic_name)
+    }
+
+    pub fn add_or_update_topic_config(
+        &mut self,
+        name: impl Into<String>,
+        config: TopicConfig,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        self.topic_config_table.insert(name.into(), config);
+        self.persist()
+    }
+
+    fn persist(&self) -> Result<(), Box<dyn std::error::Error>> {
+        fs::copy(self.path.as_str(), self.backup_path.as_str())?;
+        let data = serde_json::to_string(&self.topic_config_table)?;
+        fs::write(self.path.as_str(), data)?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use serde_json::json;
+
+    use super::*;
+    use std::sync::Mutex;
+    use once_cell::sync::Lazy;
+
+    static MTX: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
+    #[test]
+    fn test_load_config() {
+        let _m = MTX.lock();
+        let data = json!({
+            "test1": {
+                "name": "test1",
+                "topic_type": "NORMAL",
+            }
+        });
+        fs::write("./topic_config.json", data.to_string()).unwrap();
+        let mut topic_config_manager = TopicConfigManager::new("./");
+        topic_config_manager.load().unwrap();
+        let topic_config = topic_config_manager.get_topic_config("test1");
+        assert!(topic_config.is_some());
+        assert_eq!(topic_config.unwrap().name, "test1");
+        assert!(matches!(
+            topic_config.unwrap().topic_type,
+            TopicType::NORMAL
+        ));
+        fs::remove_file("./topic_config.json").unwrap();
+    }
+
+    #[test]
+    fn test_add_or_update_config() {
+        let _m = MTX.lock();
+        let mut topic_config_manager = TopicConfigManager::new("./");
+        topic_config_manager.load().unwrap();
+        let topic_config = TopicConfig {
+            name: "test1".to_string(),
+            topic_type: TopicType::NORMAL,
+        };
+        topic_config_manager
+            .add_or_update_topic_config("test1", topic_config)
+            .unwrap();
+        fs::remove_file("./topic_config.json").unwrap();
+        fs::remove_file("./topic_config.json.bak").unwrap();
+    }
+}
